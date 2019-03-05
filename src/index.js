2cc35cf31a056df27b16610a7cfe9f4e687012b7
Bálint Budavölgyi <bala92x@gmail.com>
2019-03-05 15:57:36 +0100

    Async refactoring step 1

--- src/index.js
+++ src/index.js
@@ -9,44 +9,40 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body);
     
-    user
-        .save()
-        .then(() => {
-            res.status(201).send(user);
-        })
-        .catch((err) => {
-            res.status(400).send(err);
-        });
+    try {
+        await user.save()
+        res.status(201).send(user);
+    } catch (err) {
+        res.status(400).send(err);
+    }
 });
 
-app.get('/users', (req, res) => {
-    User
-        .find({})
-        .then((users) => {
-            res.send(users);
-        })
-        .catch((err) => {
-            res.status(500).send(err);
-        });
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch (err) {
+        res.status(500).send();
+    }
 });
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
     const _id = req.params.id;
-    User
-        .findById(_id)
-        .then((user) => {
-            if (!user) {
-                return res.status(404).send();
-            }
 
-            res.send(user);
-        })
-        .catch((err) => {
-            res.status(500).send(err);
-        });
+    try {
+        const user = await User.findById(_id);
+
+        if (!user) {
+            return res.status(404).send();
+        }
+
+        res.send(user);
+    } catch (err) {
+        res.status(500).send(err);
+    }
 });
 
 app.post('/tasks', (req, res) => {
